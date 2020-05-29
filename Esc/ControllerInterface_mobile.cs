db81e4747ab9a5690c81a47a52654074957b7edf
AriESQ <19827230+AriESQ@users.noreply.github.com>
2020-05-29 22:41:48 +0000

    Initial commit

---
+++ Esc/ControllerInterface_mobile.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Text;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace Esc {
+
+	internal class ControllerInterface_mobile : System.Object {
+
+		[DllImport ("__Internal")]
+		public static extern int accessWindow ( StringBuilder id, uint idCapacity );
+
+		[DllImport ("__Internal", CharSet = CharSet.Ansi)]
+		public static extern int startDocentInterface ( string hostname );
+
+		[DllImport ("__Internal", CharSet = CharSet.Ansi)]
+		public static extern int startClientInterface ( string hostname );
+
+		[DllImport ("__Internal")]
+		public static extern int stopInterface ();
+
+		[DllImport ("__Internal")]
+		public static extern bool isConnected ();
+		
+		[DllImport ("__Internal")]
+		public static extern bool hasMoreEvents ();
+		
+		// TODO: update this in the plugin to include the StringBuilder buffer capacity input
+		[DllImport ("__Internal", CharSet = CharSet.Ansi)]
+		public static extern int getNextEvent ( StringBuilder username, uint usernameCapacity, StringBuilder message, uint messageCapacity );
+		
+		[DllImport ("__Internal")]
+		public static extern bool hasMorePresenceEvents ();
+
+		// TODO: update this in the plugin to include the StringBuilder buffer capacity input
+		[DllImport ("__Internal", CharSet = CharSet.Ansi)]
+		public static extern int getNextPresenceEvent ( StringBuilder username, uint usernameCapacity, out int presence );
+		
+		[DllImport ("__Internal")]
+		public static extern bool hasMoreStatusChanges ();
+		
+		[DllImport ("__Internal")]
+		public static extern int getNextStatusChange ( out int status );
+
+		[DllImport ("__Internal", CharSet = CharSet.Ansi)]
+		public static extern int dispatchEvent ( string username, string message );
+
+		[DllImport ("__Internal", CharSet = CharSet.Ansi)]
+		public static extern int getUniqueDeviceIdenifier ( StringBuilder id, uint idCapacity );
+
+		[DllImport ("__Internal")]
+		public static extern bool isDeviceCharging ();
+
+		[DllImport ("__Internal")]
+		public static extern bool isDeviceWifiConnected ();
+
+		[DllImport ("__Internal")]
+		public static extern bool dimScreen ();
+
+		[DllImport ("__Internal")]
+		public static extern bool brightenScreen ();
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Usage #
+
+This repo should ideally be used as a submodule in ESC game project repos, primarily the `test-project` repo
+
+
+Local path should be in Assets/Plugins/
